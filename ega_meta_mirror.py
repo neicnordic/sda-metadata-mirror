47980629e1cb85b46dfbc7f14d9ac539491da549
Stefan Negru <stefan@blankdots.com>
2020-06-22 12:24:05 +0300

    add logging to stdout

--- ega_meta_mirror.py
+++ ega_meta_mirror.py
@@ -9,10 +9,14 @@ import json
 import logging
 from pathlib import Path
 import click
+import sys
 
 FORMAT = '[%(asctime)s][%(name)s][%(process)d %(processName)s][%(levelname)-8s] (L:%(lineno)s) %(funcName)s: %(message)s'
 logging.basicConfig(format=FORMAT, datefmt='%Y-%m-%d %H:%M:%S')
+logging.StreamHandler(sys.stdout)
 LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.DEBUG)
+
 
 BASE_URL = 'https://ega-archive.org/metadata/v2/'
 ENDPOINTS = ["analyses", "dacs", "runs", "samples", "studies", "files"]
@@ -38,6 +42,7 @@ def retrieve_data(data_type: str, dataset_id: str) -> Generator:
         if r.status_code == 200:
             response = r.json()
             results_nb = int(response["response"]["numTotalResults"])
+            LOG.info(f"Retrieving {limit} {data_type} for {dataset_id} from {results_nb} results.")
             for res in process_result(response):
                 yield res
 
@@ -46,6 +51,7 @@ def retrieve_data(data_type: str, dataset_id: str) -> Generator:
             else:
                 has_more = False
         else:
+            LOG.error(f"Error retrieving {data_type} for {dataset_id}. API call returned a {r.status_code}")
             has_more = False
 
 
@@ -54,14 +60,14 @@ def process_datasets(start_limit: int = 0, defined_limit: int = 10) -> Generator
     skip: int = start_limit
     limit: int = defined_limit
     has_more = True
-    payload = {"queryBy": "dataset",
-               "skip": str(skip),
+    payload = {"skip": str(skip),
                "limit": str(limit)}
     while has_more:
         r = requests.get(f'{BASE_URL}datasets', params=payload)
         if r.status_code == 200:
             response = r.json()
             results_nb = int(response["response"]["numTotalResults"])
+            LOG.info(f"Retrieving {defined_limit} from {results_nb} results starting from {start_limit} results.")
             for res in process_result(response):
                 yield res
 
@@ -70,6 +76,7 @@ def process_datasets(start_limit: int = 0, defined_limit: int = 10) -> Generator
             else:
                 has_more = False
         else:
+            LOG.error(f"Error retrieving datasets. API call returned a {r.status_code}")
             has_more = False
 
 
@@ -77,6 +84,7 @@ def retrieve_dataset_info(dataset_id: str) -> Generator:
     """Retrieve information associated to dataset."""
     raw_events: Iterable = iter(())
     for endpoint in ENDPOINTS:
+        LOG.info(f"Processing {endpoint} for {dataset_id} ...")
         yield itertools.chain(raw_events, retrieve_data(endpoint, dataset_id))
 
 
@@ -85,6 +93,7 @@ def main(start: int = 0, limit: int = 1) -> None:
     datasets = process_datasets(start_limit=start, defined_limit=limit)
     results: Iterable = iter(())
     for dataset in datasets:
+        LOG.info(f"Processing {dataset['egaStableId']} ...")
         Path(dataset["egaStableId"]).mkdir(parents=True, exist_ok=True)
         with open(f'{dataset["egaStableId"]}/dataset_{dataset["egaStableId"]}.json', 'w') as datasetfile:
             json.dump(dataset, datasetfile)
@@ -105,8 +114,9 @@ def cli(limit_results: int, skip_results: int):
     In order to use limit the amount of requests the limit represents the number of datasets to query per run.
     Skip parameter is used to create pipelines to resume querying datasets from a specific point the dataset list.
     """
+    LOG.info(f"Start ==== >")
     main(start=skip_results, limit=limit_results)
-
+    LOG.info(f"< ==== End")
 
 if __name__ == "__main__":
     cli()
